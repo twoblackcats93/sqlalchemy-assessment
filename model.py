fe5a2cca91a180723d0d84f0892219f66554bc25
vagrant <generosa.litton@yahoo.com>
2017-03-25 19:00:22 +0000

    finsihed part 1.

---
+++ model.py
@@ -0,0 +1,94 @@
+"""Models and database functions for cars db."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+# Here's where we create the idea of our database. We're getting this through
+# the Flask-SQLAlchemy library. On db, we can find the `session`
+# object, where we do most of our interactions (like committing, etc.)
+
+db = SQLAlchemy()
+# Test relationship
+# check nullable versus not null
+
+##############################################################################
+# Part 1: Compose ORM
+
+class Brand(db.Model):
+    """Car brand."""
+
+    __tablename__ = "brands"  
+
+    brand_id = db.Column(db.String(5), primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    founded = db.Column(db.Integer, nullable=True)
+    headquarters = db.Column(db.String(50), nullable=True)
+    discontinued = db.Column(db.Integer, nullable=True)
+
+    def __repr__(self):
+        """Provide helpful representation of car results when printed."""
+
+        s = "<Brand brand_id=%s name=%s founded=%s headquarters=%s discontinued=%s>"
+        return s % (self.brand_id, self.name, self.founded, 
+                    self.headquarters, self.discontinued)    
+
+
+class Model(db.Model):
+    """Car model."""
+
+    __tablename__ = "models"
+
+    model_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    year = db.Column(db.Integer, nullable=False)
+    brand_id = db.Column(db.String(50), db.ForeignKey("brands.brand_id"), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+
+    # define relationship to brand
+    brand = db.relationship("Brand",
+                             backref=db.backref("models"))    
+
+    def __repr__(self):
+        """Provide helpful representation of model results when printed."""
+
+        s = "<Model model_id=%s year=%s brand_id=%s name=%s>"
+        return s % (self.model_id, self.year, self.brand_id, self.name)
+
+
+       
+
+# End Part 1
+
+
+##############################################################################
+# Helper functions
+
+def init_app():
+    # So that we can use Flask-SQLAlchemy, we'll make a Flask app.
+    from flask import Flask
+    app = Flask(__name__)
+
+    connect_to_db(app)
+    print "Connected to DB."
+
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our database.
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///cars'
+    app.config['SQLALCHEMY_ECHO'] = False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    # So that we can use Flask-SQLAlchemy, we'll make a Flask app.
+    from flask import Flask
+
+    app = Flask(__name__)
+
+    connect_to_db(app)
+    print "Connected to DB."
